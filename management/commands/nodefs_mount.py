773dd96d41f263a0afe20f163828f91937bea650
Felipe Ramos <perenecabuto@gmail.com>
2012-11-21 23:19:52 -0200

    Nova configiração de módulo profile por settings

--- management/commands/nodefs_mount.py
+++ management/commands/nodefs_mount.py
@@ -1,20 +1,26 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from nodefs import mounter
+import os
+
 from django.core.management.base import BaseCommand
 #from django.core.management.base import CommandError
+from django.conf import settings
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        if hasattr(settings, 'NODEFS_PROFILE_MODULE') and not os.environ.get('NODEFS_PROFILE_MODULE'):
+            os.environ['NODEFS_PROFILE_MODULE'] = settings.NODEFS_PROFILE_MODULE
 
         if len(args) < 1:
             print """
             Usage: manage.py nodefs_mount <mount point path>
             """
             sys.exit(0)
+
+        from nodefs import mounter
 
         path = args[0]
         mounter.mount(path)
